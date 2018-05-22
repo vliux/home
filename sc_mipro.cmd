5410cbcee2faf7c45818c7bf7fa2c03c94562ae2
vliux <swordmanliuxin@gmail.com>
2018-05-22 18:40:21 +0800

    Add config files for mipro

---
+++ sc_mipro.cmd
@@ -0,0 +1,11 @@
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_iphone2
+
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_nexus6p
+
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_iphone
+
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_g9300
+
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_mix2
+
+cmd /K python RunBackup.py --cfg mipro --dest sg3 --content photo_yydiphone
